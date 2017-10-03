07181c60baf2ba08109b12e81572a286d077859e
grantal <AlexV348@gmail.com>
2017-10-03 09:38:43 -0700

    made open command

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+"""
+These are all based on an old version of the tango rest api found here:
+https://github.com/autolab/Tango/wiki/Tango-REST-API/570622659163e8f7dfd032808a57a18f48006667
+I wanted to use the old version of the api docs since our server is running
+an old version of tango
+You can find the new docs here:
+https://autolab.github.io/docs/tango-rest/
+"""
 from flask import Flask
+from json import dumps
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/open/<key>/<courselab>/')
+def open(key, courselab):
+    """
+    This is the function that creates a new courselabs directory in Tango
+    <key> is of course the Tango key and <courselab> is the name of the new
+    directory
+    """
+    response = {
+        "statusMsg": "All Good",
+        "statusId": 1,
+        "files": {},
+    }
+    return dumps(response)
